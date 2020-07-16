5ff16f62d56811d2e0ff38e3216bdd75fb36c4cd
zhangx <1337842694@qq.com>
2020-07-16 21:09:51 +0800

    测试项目添加一个测试类 v3.0

---
+++ src/test/java/com/zhangxin/GithubTest.java
@@ -0,0 +1,17 @@
+package com.zhangxin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithubTest {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("-------1111------");
+    }
+
+}
